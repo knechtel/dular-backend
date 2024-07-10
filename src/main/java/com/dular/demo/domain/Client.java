e708691ffd34cb617353ab70137e5b2b3ec8c77d
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-07-10 16:13:29 -0300

    Adicionando atributos de cliente.

--- src/main/java/com/dular/demo/domain/Client.java
+++ src/main/java/com/dular/demo/domain/Client.java
@@ -1,17 +1,78 @@
 package com.dular.demo.domain;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 
 /**
  * Created by maiquelknechtel on 10/07/24.
  */
+@Entity
 public class Client {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String cpf;
     private String rg;
     private String email;
     private String address;
+    @OneToMany(mappedBy = "client",fetch = FetchType.LAZY)
     private List<Equipment> equipments;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Equipment> getEquipments() {
+        return equipments;
+    }
+
+    public void setEquipments(List<Equipment> equipments) {
+        this.equipments = equipments;
+    }
 }
