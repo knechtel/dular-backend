266654c69bd5fd94797e3f6850ee6a0419b45e83
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-07-10 15:52:43 -0300

    Adicionando a classe de usuário.

---
+++ src/main/java/com/dular/demo/domain/User.java
@@ -0,0 +1,45 @@
+package com.dular.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * Created by maiquelknechtel on 10/07/24.
+ */
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String login;
+    private String password;
+
+    public Integer getId() {
+
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
