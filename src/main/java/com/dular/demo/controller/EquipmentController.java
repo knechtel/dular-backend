a5a97ae69d2e71f8fab83cf39a5b203160509279
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-07-15 12:32:50 -0300

    Adicionando controller de Aparelhos.

--- src/main/java/com/dular/demo/controller/EquipmentController.java
+++ src/main/java/com/dular/demo/controller/EquipmentController.java
@@ -1,4 +1,52 @@
-package com.dular.demo.controller;/**
+package com.dular.demo.controller;
+
+import com.dular.demo.DTO.EquipmentDto;
+import com.dular.demo.domain.Equipment;
+import com.dular.demo.service.ClientService;
+import com.dular.demo.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+/**
  * Created by maiquelknechtel on 13/07/24.
- */public class EquipmentController {
+ */
+@RestController
+public class EquipmentController {
+    @Autowired
+    private EquipmentService equipmentService;
+    @Autowired
+    private ClientService clientService;
+
+    @CrossOrigin(origins = "http://127.0.0.1:5173")
+    @PostMapping(value = "equipment-create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Equipment create(@RequestBody EquipmentDto equipmentDto){
+        Equipment equipment = equipmentDto.toBuild(equipmentDto);
+        if(equipmentDto.getIdClient()!=null)
+            equipment.setClient(clientService.findById(equipmentDto.getIdClient()));
+        equipment.setEntryDate(new Date());
+        return equipmentService.create(equipment);
+    }
+
+    @RequestMapping(value = "equipment-findAll", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public List<Equipment> findAll(){
+        return equipmentService.findAll();
+    }
+
+    @RequestMapping(value = "equipment-update", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public void update(@RequestBody EquipmentDto equipmentDto){
+        equipmentService.update(equipmentDto.toBuildUpdate(equipmentDto));
+    }
+    @PostMapping(value = "equipment-delete", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@RequestBody EquipmentDto equipmentDto){
+        equipmentService.delete(equipmentDto.toBuild(equipmentDto));
+    }
+
+    @PostMapping(value = "equipment-findById", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Equipment findById(@RequestBody EquipmentDto equipmentDto){
+        return equipmentService.findById(equipmentDto.getId());
+    }
 }
