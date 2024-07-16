efa8044b05201f73647c3c17060c89a20f11391a
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-07-16 13:42:30 -0300

    Adicionando método de serviço.

--- src/main/java/com/dular/demo/service/UserProfileService.java
+++ src/main/java/com/dular/demo/service/UserProfileService.java
@@ -1,7 +1,19 @@
 package com.dular.demo.service;
 
+import com.dular.demo.domain.UserProfile;
+import com.dular.demo.repository.UserProfileDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by maiquelknechtel on 16/07/24.
  */
+@Service
 public class UserProfileService {
+    @Autowired
+    private UserProfileDao userProfileDao;
+
+    public UserProfile save(UserProfile userProfile){
+        return userProfileDao.save(userProfile);
+    }
 }
